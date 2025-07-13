d370370fab24ae610373fe3309ae5be1d9b66f5f
Azibi Fethi <azibi.fethi99@gmail.com>
2025-07-13 23:44:22 +0100

    edit main to include base

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from fastapi import fastapi
+from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv(".env")
+
+from routes import base
 
 app = FastAPI()
 
-@app.get("/")
-def welcome():
-    return {"Hello": "World"}
+app.include_router(base.base_router)
